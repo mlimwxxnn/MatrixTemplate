4da9e909f9527490aae39e160a501a1e1e4d5de9
GitHub Action <action@github.com>
2021-08-11 13:19:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/mlimwxxnn/matrixtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/mlimwxxnn/matrixtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mlimwxxnn.matrixtemplate
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mlimwxxnn/matrixtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mlimwxxnn/matrixtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mlimwxxnn.matrixtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mlimwxxnn.matrixtemplate.MyBundle
 
 class MyApplicationService {
 
